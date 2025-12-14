1dbde78ffd539e916066aebe9cc291e159f2b03d
devopswihnaveen <naveen.velanati1998@gmail.com>
2025-12-14 13:13:29 +0530

    Removed

--- myself.java
+++ myself.java
@@ -1,13 +1,1 @@
-public class SelfIntroduction {
-    public static void main(String[] args) {
-        String name = "Velanati Naveen Kumar";
-        int age = 25;
-        String profession = "Devops Engineer";
-        String hobby = "Develop and Deploy Applications";
-        
-        System.out.println("Hello, my name is " + name);
-        System.out.println("I am " + age + " years old");
-        System.out.println("I work as a " + profession);
-        System.out.println("My hobby is " + hobby);
-    }
-}
+
